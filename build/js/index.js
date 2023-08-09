c137637ac8ad92c31ff822bde918af13ffb579f6
Daniel Yesid Cely Robayo <danielcely1@gmail.com>
2023-08-09 13:46:50 -0500

    galeria en proceso

--- build/js/index.js
+++ build/js/index.js
@@ -14,17 +14,41 @@ function iniciarApp(){
 
 
 function crearGaleria(){
-    const galeria = document.getElementById('galeria__imagenes')
+
+    let galeria = document.querySelector('.galeria-imagenes');
 
     for(let i = 1; i <= 12; i++){
+        let campo = document.createElement('li');
         let img = document.createElement('picture');
-        img.innerHTML = `
-                        <source srcset="./build/img/thumb/${i}.avif" type="image/webp">
-                        <source srcset="./build/img/thumb/${i}.webp" type="image/avif">
-                        <img loading="lazy" src="./src/img/thumb/${i}.jpg" alt={i}>
-        `;
-        galeria.appendChild(img)
+        img.innerHTML = ` 
+            <source srcset="/build/img/thumb/${i}.webp" type="image/webp">
+            <source srcset="/build/img/thumb/${i}.avif" type="image/avif">
+            <img loading="lazy" src="/build/img/thumb/${i}.jpg" alt="vocalista">`
+
+            img.addEventListener('click', () =>{
+                mostrarImagen(i)
+            })
+
+        campo.appendChild(img)
+        galeria.appendChild(campo)
     }
+}
 
 
+
+
+function mostrarImagen(id){
+    const imagen = document.createElement('picture')
+    imagen.innerHTML = ` 
+            <source srcset="/build/img/grande/${id}.webp" type="image/webp">
+            <source srcset="/build/img/grande/${id}.avif" type="image/avif">
+            <img loading="lazy" src="/build/img/grande/${id}.jpg" alt="vocalista">`
+
+      const overlay = document.createElement('div')
+      overlay.appendChild(imagen)
+      overlay.classList.add('overlay')
+      
+      const body = document.querySelector('body')
+      body.appendChild(overlay)
+    console.log(`mostrando id: ${id}`)
 }
