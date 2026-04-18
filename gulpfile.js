38608cb8502cbbf7b60b682c04b4f29c0adb5235
dandy182 <danielcely1@gmail.com>
2026-04-17 19:07:20 -0500

    agregado de sass, gulp y gulpSass

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,12 @@
-import {src, dest, series} from 'gulp';
+import {src, dest, series} from 'gulp'
+import * as dartSass from 'sass'
+import gulpSass from 'gulp-sass';
+
+const sass = gulpSass(dartSass);
+
+
+export function compilarCss(done){
+    src('./src/sass/app.scss')
+        .pipe(sass().on(Error. sass.Error()))
+        .pipe(dest('./build/css'))
+}
